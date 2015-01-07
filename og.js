2a3229888fd9ea4c6dc2d5b058789df6027ee5f0
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-01-06 23:06:58 -0500

    Add og_field_widget_form() with support for entity reference widgets.

--- og.js
+++ og.js
@@ -1,6 +1,27 @@
 /**
  * HOOKS
  */
+
+/**
+ * Implements hook_field_widget_form().
+ */
+function og_field_widget_form(form, form_state, field, instance, langcode, items, delta, element) {
+  try {
+    //console.log(field);
+    //console.log(instance);
+    switch (instance.widget.type) {
+      case 'og_complex':
+        // Just forward this widget to the entity reference module.
+        entityreference_field_widget_form(form, form_state, field, instance, langcode, items, delta, element);
+        break;
+      default:
+        console.log('og_field_widget_form - unknown widget type (' + instance.widget.type + ')');
+        return;
+        break;
+    }
+  }
+  catch (error) { console.log('og_field_widget_form - ' + error); }
+}
  
 /**
  * SERVICES
@@ -15,3 +36,4 @@ function og_user_groups(uid, options) {
   }
   catch (error) { console.log('og_user_groups - ' + error); }
 }
+
