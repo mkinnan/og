b5a435ff32b8c7a975f97e552d1b0b4c5c1bf2c5
mkinnan <mark@saponico.com>
2017-07-15 17:17:32 -0600

    Update
    Add Try to all functions. Add author uid to disable join/leave button for group creator.

--- og.js
+++ og.js
@@ -9,18 +9,48 @@
  * @returns {boolean}
  */
 function og_is_member(gid) {
-  var member = false;
-  var groups = og_config('user_groups');
-  if (groups && groups.length) {
-    for (var i = 0; i < groups.length; i++) {
-      var group = groups[i];
-      if (group.gid == gid) {
-        member = true;
-        break;
+  try {
+  
+    var member = false;
+    var groups = og_config('user_groups');
+    if (groups && groups.length) {
+      for (var i = 0; i < groups.length; i++) {
+        var group = groups[i];
+        if (group.gid == gid) {
+          member = true;
+          break;
+        }
       }
-    }
+    }    
+    
+    return member;
+    
   }
-  return member;
+  catch (error) { console.log('og_is_member - ' + error); }
+}
+
+function og_is_author(gid) {
+  try {
+  
+    var author = false;
+    var groups = og_config('user_groups');
+    if (groups && groups.length) {
+      for (var i = 0; i < groups.length; i++) {
+        var group = groups[i];
+        if (group.gid == gid) {
+          if (group.uid == Drupal.user.uid) {
+            console.log(group.uid);
+            author = true;
+            break;
+          }
+        }
+      }
+    }    
+    
+    return author;
+    
+  }
+  catch (error) { console.log('og_is_author - ' + error); }
 }
 
 /**
@@ -31,24 +61,33 @@ function og_is_member(gid) {
  * Implements hook_install().
  */
 function og_install() {
-  drupalgap.og = {
-    user_groups: null
-  };
+  try {
+    drupalgap.og = {
+      user_groups: null
+    };
+  }
+  catch (error) { console.log('og_install - ' + error); }
 }
 
 function og_config(key, value) {
-  if (typeof value !== 'undefined') { drupalgap.og[key] = value; }
-  else { return drupalgap.og[key]; }
+  try {
+    if (typeof value !== 'undefined') { drupalgap.og[key] = value; }
+    else { return drupalgap.og[key]; }
+  }
+  catch (error) { console.log('og_config - ' + error); }
 }
 
 /**
  * Implements hook_services_request_pre_postprocess_alter().
  */
 function og_services_request_pre_postprocess_alter(options, result) {
-  // Set aside their groups.
-  if (options.service == 'user' && options.resource == 'groups') {
-    og_config('user_groups', result);
+  try {
+    // Set aside their groups.
+    if (options.service == 'user' && options.resource == 'groups') {
+      og_config('user_groups', result);
+    }
   }
+  catch (error) { console.log('og_services_request_pre_postprocess_alter - ' + error); }
 }
 
 /**
@@ -65,19 +104,11 @@ function og_services_postprocess(options, result) {
       }
     }
   }
-  catch (error) {
-    console.log('og_services_postprocess - ' + error);
-  }
+  catch (error) { console.log('og_services_postprocess - ' + error); }
 }
 
 function og_ui_field_formatter_view(entity_type, entity, field, instance, langcode, items, display) {
   try {
-
-    //console.log(entity_type);
-    //console.log(entity);
-    //console.log(field);
-    //console.log(instance);
-    //console.log(display);
 
     var element = {};
     $.each(items, function(delta, item) {
@@ -88,7 +119,7 @@ function og_ui_field_formatter_view(entity_type, entity, field, instance, langco
             // Build the subscription button.
             var gid = entity[entity_primary_key(entity_type)];
             element[delta] = { theme: 'og_group_subscribe' };
-            var props = og_group_subscribe_button_properties(gid, og_is_member(gid));
+            var props = og_group_subscribe_button_properties(gid, og_is_member(gid), og_is_author(gid));
             $.extend(element[delta], props, true);
 
           break;
@@ -101,22 +132,31 @@ function og_ui_field_formatter_view(entity_type, entity, field, instance, langco
       //console.log(item);
 
     });
+    
     return element;
+    
   }
   catch (error) { console.log('og_ui_field_formatter_view - ' + error); }
 }
 
-function og_group_subscribe_button_properties(gid, member) {
-  return {
-    text: !member ? 'Subscribe to group' : 'Unsubscribe from group',
-    path: null,
-    member: member,
-    options: {
-      attributes: {
-        onclick: '_og_group_subscribe_click(this, ' + gid + ', ' + member + ')'
+function og_group_subscribe_button_properties(gid, member, author) {
+  try {
+
+    return {
+      text: !author ? ( !member ? 'Join Group' : 'Leave Group' ) : 'You are the Group Manager',
+      path: null,
+      member: member,
+      author: author,
+      options: {
+        attributes: {
+          onclick: !author ? '_og_group_subscribe_click(this, ' + gid + ', ' + member + ')' : '',
+          class: !author ? ( !member ? 'og-subscribe ui-link ui-btn ui-shadow ui-corner-all' : 'og-unsubscribe ui-link ui-btn ui-shadow ui-corner-all' ) : 'og-author ui-link ui-btn ui-shadow ui-corner-all'
+        }
       }
-    }
-  };
+    };
+  
+  }
+  catch (error) { console.log('og_group_subscribe_button_properties - ' + error); }
 }
 
 /**
@@ -125,7 +165,12 @@ function og_group_subscribe_button_properties(gid, member) {
  * @returns {String}
  */
 function theme_og_group_subscribe(variables) {
-  return bl(variables.text, variables.path, variables.options);
+  try {
+    
+    return bl(variables.text, variables.path, variables.options);
+  
+  }
+  catch (error) { console.log('theme_og_group_subscribe - ' + error); }
 }
 
 /**
@@ -138,13 +183,19 @@ function theme_og_group_subscribe(variables) {
  * @private
  */
 function _og_group_subscribe_click(button, gid, member) {
-  var done = function(result) {
-    var props = og_group_subscribe_button_properties(gid, og_is_member(gid));
-    $(button).text(props.text);
-    $(button).attr('onclick', props.options.attributes.onclick).trigger('create');
-  };
-  !member ? og_user_join(gid, Drupal.user.uid, { success: done }) :
-      og_user_leave(gid, Drupal.user.uid, { success: done });
+  try {
+  
+    var done = function(result) {
+      var props = og_group_subscribe_button_properties(gid, og_is_member(gid), og_is_author(gid));
+      $(button).text(props.text);
+      $(button).attr('onclick', props.options.attributes.onclick).trigger('create');
+      $(button).attr('class', props.options.attributes.class).trigger('create');
+    };
+    !member ? og_user_join(gid, Drupal.user.uid, { success: done }) :
+        og_user_leave(gid, Drupal.user.uid, { success: done });
+      
+  }
+  catch (error) { console.log('_og_group_subscribe_click - ' + error); }
 }
 
 /**
@@ -152,6 +203,7 @@ function _og_group_subscribe_click(button, gid, member) {
  */
 function og_field_widget_form(form, form_state, field, instance, langcode, items, delta, element) {
   try {
+    
     //console.log(field);
     //console.log(instance);
     switch (instance.widget.type) {
@@ -164,6 +216,7 @@ function og_field_widget_form(form, form_state, field, instance, langcode, items
         return;
         break;
     }
+    
   }
   catch (error) { console.log('og_field_widget_form - ' + error); }
 }
@@ -171,12 +224,12 @@ function og_field_widget_form(form, form_state, field, instance, langcode, items
 /**
  * Implements hook_assemble_form_state_into_field().
  */
-function og_assemble_form_state_into_field(entity_type, bundle,
-  form_state_value, field, instance, langcode, delta, field_key) {
+function og_assemble_form_state_into_field(entity_type, bundle, form_state_value, field, instance, langcode, delta, field_key) {
   try {
+    
     // Just forward this field's assembly to the entity reference module.
-    return entityreference_assemble_form_state_into_field(entity_type, bundle,
-      form_state_value, field, instance, langcode, delta, field_key);
+    return entityreference_assemble_form_state_into_field(entity_type, bundle, form_state_value, field, instance, langcode, delta, field_key);
+    
   }
   catch (error) { console.log('entityreference_assemble_form_state_into_field - ' + error); }
 }
@@ -208,17 +261,22 @@ function og_user_groups(uid, options) {
  * @param options
  */
 function og_user_join(gid, uid, options) {
-  var success = options.success.bind({});
-  options.method = 'POST';
-  options.path = 'og/join/' + gid + '/' + uid;
-  options.service = 'og';
-  options.resource = 'user_join';
-  options.success = function(result) {
-    og_user_groups(Drupal.user.uid, {
-      success: function(groups) { success(result); }
-    });
-  };
-  Drupal.services.call(options);
+  try {
+        
+    var success = options.success.bind({});
+    options.method = 'POST';
+    options.path = 'og/join/' + gid + '/' + uid;
+    options.service = 'og';
+    options.resource = 'user_join';
+    options.success = function(result) {
+      og_user_groups(Drupal.user.uid, {
+        success: function(groups) { success(result); }
+      });
+    };
+    Drupal.services.call(options);
+  
+  }
+  catch (error) { console.log('og_user_join - ' + error); }
 }
 
 /**
@@ -228,66 +286,20 @@ function og_user_join(gid, uid, options) {
  * @param options
  */
 function og_user_leave(gid, uid, options) {
-  var success = options.success.bind({});
-  options.method = 'POST';
-  options.path = 'og/leave/' + gid + '/' + uid;
-  options.service = 'og';
-  options.resource = 'user_leave';
-  options.success = function(result) {
-    og_user_groups(Drupal.user.uid, {
-      success: function(groups) { success(result); }
-    });
-  };
-  Drupal.services.call(options);
-}
-
-/**
- * MEMBERSHIP PROXIES
- */
-function og_membership_load(id, options) {
-  og_membership_retrieve(id, options);
-}
-
-function og_membership_save(membership, options) {
-  !membership.id ?
-      og_membership_create(membership, options) :
-      og_membership_update(membership, options);
-}
-
-/**
- * MEMBERSHIP SERVICES
- */
-
-function og_membership_create(membership, options) {
-  options.method = 'POST';
-  options.path = 'og_membership.json';
-  options.service = 'og_membership';
-  options.resource = 'create';
-  options.data = JSON.stringify(membership);
-  Drupal.services.call(options);
-}
-
-function og_membership_retrieve(id, options) {
-  options.method = 'GET';
-  options.path = 'og_membership/' + id + '.json';
-  options.service = 'og_membership';
-  options.resource = 'retrieve';
-  Drupal.services.call(options);
-}
-
-function og_membership_update(membership, options) {
-  options.method = 'PUT';
-  options.path = 'og_membership/' + membership.id + '.json';
-  options.service = 'og_membership';
-  options.resource = 'update';
-  options.data = JSON.stringify(membership);
-  Drupal.services.call(options);
-}
-
-function og_membership_delete(id, options) {
-  options.method = 'DELETE';
-  options.path = 'og_membership/' + id + '.json';
-  options.service = 'og_membership';
-  options.resource = 'delete';
-  Drupal.services.call(options);
+  try {
+    
+    var success = options.success.bind({});
+    options.method = 'POST';
+    options.path = 'og/leave/' + gid + '/' + uid;
+    options.service = 'og';
+    options.resource = 'user_leave';
+    options.success = function(result) {
+      og_user_groups(Drupal.user.uid, {
+        success: function(groups) { success(result); }
+      });
+    };
+    Drupal.services.call(options);
+  
+  }
+  catch (error) { console.log('og_user_leave - ' + error); }
 }
